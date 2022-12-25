5426acf0bb9cc733b3acb99307d825c3f51e0f1a
Minisiia <ar_tik@yahoo.com>
2022-12-25 23:27:47 +0200

    Java Advanced 5. Reflection

---
+++ src/access/Animal.java
@@ -0,0 +1,17 @@
+package access;
+
+public class Animal {
+    public String name = "Kicky";
+    private int age = 2;
+    protected int weight = 3;
+    String breed = "Unknown";
+
+    @Override
+    public String toString() {
+        return "Animal\t " +
+                "name: " + name +
+                ", age: " + age +
+                ", weight: " + weight +
+                ", breed: " + breed;
+    }
+}
